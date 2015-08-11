c7ee40225e3e1dc803ceb85a37ae48c2265015a4
Devon Jones <devon.jones@sendgrid.com>
2015-08-10 18:34:38 -0600

    added more to script

--- launch.sh
+++ launch.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+apt-get update
+apt-get install -y python-pip wget
+
+wget -qO- https://get.docker.com/ | sh
+
 ./logstash-conf.py > logstash-conf/logstash.conf
 
+pip install docker-compose
+
+docker-compose up
